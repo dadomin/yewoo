46c4f2901b5b7163fceafd52e530d7c11fc2ca73
dadomin <brlight_dm@naver.com>
2020-09-09 02:39:11 +0900

    20200908 화요일

---
+++ views/company.php
@@ -0,0 +1,45 @@
+<ul id="model-top-menu">
+    <li name="company-intro">ABOUT US</li>
+    <li name="company-leadership">LEADERSHIP</li>
+    <li name="company-family">FAMILY</li>
+    <li name="company-gallery">GALLERY</li>
+    <li name="company-contact">CONTACT</li>
+</ul>
+
+<section id="company-title">
+    
+    <div id="circle-1" class="circle"></div>
+    <div id="circle-2" class="circle"></div>
+    <div id="circle-3" class="circle"></div>
+    <h3>
+        MUSIC & ARTIST <br><span></span><br>
+        FOR HEALING
+    </h3>
+    <h2>
+        <img src="/img/page/logo-white.png" alt=""><br>
+        예우엔터테인먼트</h2>
+</section>
+
+<section id="company-intro">
+    <h3>WHO WE ARE</h3>
+    <div>
+        <p>빅히트 엔터테인먼트는 “Music & Artist for Healing, 음악과 아티스트를 통해 사람들에게 위로와 감동을 준다” 라는 미션 아래 음악 산업의 비즈니스 모델을 혁신하는 기업입니다. 빅히트는 음악에 기반한 세계 최고 수준의 엔터테인먼트 라이프스타일 플랫폼 기업을 지향합니다.</p>
+        
+        <p>글로벌 트렌드를 이끄는 '콘텐츠'와 우리의 고객인 '팬'을 최우선 가치로 두고, 높은 기준과 끊임없는 개선으로 고객을 만족시키도록 노력하고 있습니다.</p>
+        <p>
+        빅히트 소속 아티스트로는 '21세기 비틀스'라 일컬어지는 그룹
+        방탄소년단(BTS)과 '글로벌 슈퍼루키' 투모로우바이투게더(TXT), '감성 발라더' 이현이 있습니다.</p>
+    </div>
+</section>
+
+<section id="company-leadership">
+</section>
+
+<section id="company-family">
+</section>
+
+<section id="company-gallery"></section>
+
+<section id="company-contact"></section>
+
+<script src="/js/model.js"></script>
